4d19521430abdb4c108f275c182c0612d34bcc0d
Krzysztof <82186993+KrzOle002@users.noreply.github.com>
2025-01-22 12:14:08 +0100

    initial commit

---
+++ src/test/java/com/krzole/manager_tools/ManagerToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzole.manager_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
